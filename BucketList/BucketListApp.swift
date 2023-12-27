2e08d35cc5916f3b71e12e2b82dcd2b0c88591f4
sudhirke <sudhir.kesharwani@gmail.com>
2023-12-27 21:58:08 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Sudhir Kesharwani on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Sudhir Kesharwani on 27/12/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct Users: Comparable,Identifiable{
+    var id = UUID()
+    var firstName: String
+    var lastName: String
+    
+    static func <(lhs: Users, rhs: Users) -> Bool{
+        lhs.lastName < rhs.lastName
+    }
+}
+
+struct ContentView: View {
+    
+    let users = [
+    
+        Users(firstName: "Abhishek", lastName: "Banerjee"),
+        Users(firstName: "Sudhir", lastName: "Kesharwani"),
+        Users(firstName: "Khozema", lastName: "Rangwala"),
+        Users(firstName: "Huzefa", lastName: "Andmanwala"),
+    ].sorted()
+    
+    var body: some View {
+        VStack {
+            Map()
+                .cornerRadius(10)
+            List(users){ user in
+                Text("\(user.lastName), \(user.firstName)")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
